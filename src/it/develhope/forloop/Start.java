d6bc6dce7b1b12cc8a06d0beb7ff040dd48e4e59
Alessio Limina <alessio.limina@gmail.com>
2023-02-08 21:50:28 +0100

    Exercise For Loop 03

--- src/it/develhope/forloop/Start.java
+++ src/it/develhope/forloop/Start.java
@@ -8,7 +8,7 @@ public class Start {
         String[] arr = new String[]{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 
         for (int i = 0; i < arr.length; i++) {
-            if (i % 2 != 0)
+            if (i % 2 == 0)
                 {System.out.println(arr[i].toUpperCase());}
                 else {System.out.println(arr[i]);}}
 
